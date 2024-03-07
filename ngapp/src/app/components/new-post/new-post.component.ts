5ea29a3a1c78774a6b51be3d0ea111c519038c9c
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-03-07 17:19:45 +0000

    init angular clone fo twitter

---
+++ ngapp/src/app/components/new-post/new-post.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-post.component.html',
+  styleUrl: './new-post.component.css'
+})
+export class NewPostComponent {
+  tweetMaxLength:number = 240
+  tweetRemain:number = this.tweetMaxLength;
+}
