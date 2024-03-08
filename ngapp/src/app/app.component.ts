882d7f80398bbe2d825a1d2eea31dfb9962afd96
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-03-08 18:02:36 +0000

    add new func

--- ngapp/src/app/app.component.ts
+++ ngapp/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from "./components/footer/footer.component";
 import { NewPostComponent } from "./components/new-post/new-post.component";
 import { FormsModule } from '@angular/forms';
+import { TweetComponent } from "./components/tweet/tweet.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, NewPostComponent,FormsModule]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, NewPostComponent, FormsModule, TweetComponent]
 })
 export class AppComponent {
   title = 'ngapp';
