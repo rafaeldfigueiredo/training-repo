1b86515e889e5ccfad0ff7c1b16eeca3157ca7b6
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-03-13 18:11:14 -0300

    new edits add

--- ngapp/src/app/components/tweet/tweet.component.ts
+++ ngapp/src/app/components/tweet/tweet.component.ts
@@ -8,9 +8,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tweet.component.css',
 })
 export class TweetComponent {
-<<<<<<< HEAD
- @Input() TweetMessage: string = '';
-=======
   @Input() TweetMessage: string = '';
->>>>>>> cfa9fec37c2f62810fc05017936ce9f282beeb6e
 }
